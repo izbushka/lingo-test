06f98cf20623ebff18d85c5eda5f8802b3a37a0d
Oleg Muraviov <oleg.muraviov@kpt.kiev.ua>
2020-04-10 08:25:13 +0300

    left menu done

--- src/app/shared/layouts/main-layout/navbar/navbar.component.ts
+++ src/app/shared/layouts/main-layout/navbar/navbar.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CarsService} from '../../../services/cars.service';
 import {Car} from '../../../interfaces/car';
+import {takeWhile} from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  favorites: Car[];
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAlive = true;
+  favorites: Car[] = [];
 
   constructor(private carsService: CarsService) { }
 
   ngOnInit(): void {
+    this.carsService.getFavorites().pipe(
+      takeWhile(() => this.isAlive)
+    ).subscribe(
+      data => {
+        this.favorites = [];
+        data.forEach(id => {
+          // console.log(id, this.carsService.getCar(id));
+          this.favorites.push(this.carsService.getCar(id));
+        });
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.isAlive = false;
   }
 
 }
--- src/app/shared/services/cars.service.ts
+++ src/app/shared/services/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 
 import CarsMockData from '../../cars-model.json';
@@ -13,17 +13,20 @@ export class CarsService {
   private cars$: BehaviorSubject<Car[]> = new BehaviorSubject([]);
   private favorites$: BehaviorSubject<number[]> = new BehaviorSubject([]);
 
-  constructor() { }
+  constructor() {
+  }
 
   getCars(): BehaviorSubject<Car[]> {
-    if (this.cars$.getValue().length === 0) {
+    if (this.cars$.getValue().length === 0) { // first call -> get ajax data
       this._httpGetCars().subscribe(data => this.cars$.next(data));
     }
     return this.cars$;
   }
 
-  _httpGetCars(): Observable<Car[]> {
-    return of(CarsMockData).pipe(delay(1000));
+  getCar(id: number): Car {
+    const car = this.cars$.getValue().filter(item => item.id === id).shift();
+    return car;
+    // return this.cars$.getValue().filter(car => car.id === id).shift();
   }
 
   getFavorites(): BehaviorSubject<number[]> {
@@ -38,7 +41,11 @@ export class CarsService {
   }
 
   removeFromFavorites(id: number): void {
-    const list = this.favorites$.getValue().filter(i => i === id);
+    const list = this.favorites$.getValue().filter(i => i !== id);
     this.favorites$.next(list);
+  }
+
+  private _httpGetCars(): Observable<Car[]> {
+    return of(CarsMockData).pipe(delay(1000));
   }
 }
